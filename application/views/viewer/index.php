d2527e1f39b8e9abffc6629cd8c7066a377971f5
Jan Zarsky <jan.zarsky@gmail.com>
2014-03-07 11:43:26 +0100

    Added basic styling

--- application/views/viewer/index.php
+++ application/views/viewer/index.php
@@ -1,9 +1,12 @@
 <section class="player">
+	<div class="player__skills">
+		<div class="player__skills__bar" style="height: <?php echo $player['skill_power']; ?>em">&nbsp;</div>
+		<div class="player__skills__label"><?php echo $player['skill_power']; ?></div>
+		<div class="player__skills__bar" style="height: <?php echo $player['skill_mind']; ?>em">&nbsp;</div>
+		<div class="player__skills__label"><?php echo $player['skill_mind']; ?></div>
+		<div class="player__skills__bar" style="height: <?php echo $player['skill_magic']; ?>em">&nbsp;</div>
+		<div class="player__skills__label"><?php echo $player['skill_magic']; ?></div>
+	</div>
 	<header class="player__name"><?php echo $player['name']; ?></header>
 	<div class="player__score"><?php echo $player['score']; ?></div>
-	<div class="player__skills">
-		<div class="player__skills__power"><?php echo $player['skill_power']; ?></div>
-		<div class="player__skills__mind"><?php echo $player['skill_mind']; ?></div>
-		<div class="player__skills__magic"><?php echo $player['skill_magic']; ?></div>
-	</div>
 </section>
