541c5a415d027583eaea3ca9b6c9ba66b6b65134
Jan Zarsky <jan.zarsky@gmail.com>
2014-03-19 10:23:20 +0100

    Added submitting new players to db

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -24,4 +24,45 @@ class Admin_model extends CI_Model {
 			
 		return $this->db->get()->result_array();
 	}
+	
+	public function add_player($name, $avatar_id)
+	{
+		$players_data = array(
+			'name' => $name,
+			'avatar_id' => $avatar_id
+		);
+		
+		try {
+			$this->db->insert('players', $players_data);
+		}
+		catch (Exception $e) {
+			throw new Exception('dberror');
+		}
+		
+		for ($i = 1; $i <= 3; $i++) {
+			$players_skills_data[$i] = array(
+				'player_id' => $this->db->insert_id(),
+				'skill_id' => $i,
+				'value' => 100
+			);
+		}
+		
+		try {
+			$this->db->insert_batch('players_skills', $players_skills_data);
+		}
+		catch (Exception $e) {
+			throw new Exception('dberror');
+		}
+		
+		$avatars_data = array(
+			'free' => 0
+		);
+		
+		try {
+			$this->db->update('avatars', $avatars_data, 'id = ' . $avatar_id);
+		}
+		catch (Exception $e) {
+			throw new Exception('dberror');
+		}
+	}
 }
