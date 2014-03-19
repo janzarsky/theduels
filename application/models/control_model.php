256138165496c0bc5afc681544ee86f7239b9a59
Jan Zarsky <jan.zarsky@gmail.com>
2014-03-19 11:48:05 +0100

    Added score computing

--- application/models/control_model.php
+++ application/models/control_model.php
@@ -28,7 +28,43 @@ class Control_model extends CI_Model {
 		if ($player_1_id == $player_2_id)
 			throw new Exception("players are the same");
 		
+		$skill_id = $this->get_skill_id($game_id);
+		
+		$player_1_skill = $this->get_skill($player_1_id, $skill_id);
+		$player_2_skill = $this->get_skill($player_2_id, $skill_id);
+		
+		$expected_score_1 = $this->count_score($player_1_skill, $player_2_skill);
+		$expected_score_2 = 1 - $expected_score_1;
+		
+		$score_1 = $score/2;
+		$score_2 = 1 - $score/2;
+		
+		$k = 10;
+		
+		$player_1_skill += $k*($expected_score_1 - $score_1);
+		$player_2_skill += $k*($expected_score_2 - $score_2);
+		
+		$this->update_skill($player_1_id, $skill_id, $player_1_skill);
+		$this->update_skill($player_2_id, $skill_id, $player_2_skill);
+		
 		$this->log_duel($game_id, $player_1_id, $player_2_id, $score);
+	}
+	
+	public function get_skill_id($game_id) {
+		return $this->db->select('skill_id')->from('games')->where('id', $game_id)->get()->row_array()['skill_id'];
+	}
+	
+	private function get_skill($player_id, $skill_id) {
+		return $this->db->select('value')->from('players_skills')
+			->where(array('player_id' => $player_id, 'skill_id' => $skill_id))->get()->row_array()['value'];
+	}
+	
+	private function count_score($p1, $p2) {
+		return 1/(1 + pow(10, ($p2 - $p1)/40));
+	}
+	
+	private function update_skill($player_id, $skill_id, $skill_value) {
+		$this->db->update('players_skills', array('value' => $skill_value), array('player_id' => $player_id, 'skill_id' => $skill_id));
 	}
 	
 	private function log_duel($game_id, $player_1_id, $player_2_id, $score) {
