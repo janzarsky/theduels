09e11312fcea70c208c86689870fcebd07667eba
Jan Zarsky <jan.zarsky@gmail.com>
2014-03-19 09:54:10 +0100

    Overview fix

--- application/views/overview/index.php
+++ application/views/overview/index.php
@@ -1,7 +1,7 @@
 <div class="content">
 	<?php foreach ($players as $player): ?>
 		<div class="player" id="player<?php echo $player['id']; ?>">
-			<img class="player__image" src="/theduely/media/images/avatars/<?php echo $player['avatar']; ?>.png">
+			<img class="player__image" src="/theduely/media/images/avatars/<?php echo $player['number']; ?>.png">
 			<div class="player__name">
 				<?php echo $player['name']; ?>
 			</div>
