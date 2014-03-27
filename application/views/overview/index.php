27349d69a64e21ec8d13f71fa9143d3daffea691
Jan Zarsky <jan.zarsky@gmail.com>
2014-03-27 16:26:16 +0100

    Added link to viewer in overview page

--- application/views/overview/index.php
+++ application/views/overview/index.php
@@ -1,11 +1,13 @@
 <div class="content">
 	<?php foreach ($players as $player): ?>
-		<div class="player" id="player<?php echo $player['playerid']; ?>">
-			<img class="player__image" src="/theduely/media/images/avatars/<?php echo $player['number']; ?>.png">
-			<div class="player__name">
-				<?php echo $player['name']; ?>
-			</div>
+		<a href="<?php echo base_url('viewer/' . $player['playerid']); ?>">
+			<div class="player" id="player<?php echo $player['playerid']; ?>">
+				<img class="player__image" src="/theduely/media/images/avatars/<?php echo $player['number']; ?>.png">
+				<div class="player__name">
+					<?php echo $player['name']; ?>
+				</div>
 		</div>
+		</a>
 	<?php endforeach; ?>
 </div>
 
