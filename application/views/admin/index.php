f2e0527515b2b51d49424a6966c25a2f5235d0a4
Jan Zarsky <jan.zarsky@gmail.com>
2014-03-28 14:54:52 +0100

    Added admin index page

---
+++ application/views/admin/index.php
@@ -0,0 +1,25 @@
+<div class="pages">
+	<header class="pages__header">
+		Stránky:
+	</header>
+	<ul class="pages__list">
+			<a href="<?php echo base_url('/control'); ?>">
+				<li class="pages__list__item">
+					Zadávání duelů
+				</li>
+			</a>
+			<a href="<?php echo base_url('/admin/addplayers'); ?>">
+				<li class="pages__list__item">
+					Správa hráčů
+				</li>
+			</a><a href="<?php echo base_url('/overview'); ?>">
+				<li class="pages__list__item">
+					Přehled
+				</li>
+			</a><a href="<?php echo base_url('/viewer'); ?>">
+				<li class="pages__list__item">
+					Prohlížeč
+				</li>
+			</a>
+	</ul>
+</div>
