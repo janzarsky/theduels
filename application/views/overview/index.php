337592fc4897c1b334ba032d3d2eedbceba420af
Jan Zarsky <jan.zarsky@gmail.com>
2014-03-31 19:10:25 +0200

    Refactoring

--- application/views/overview/index.php
+++ application/views/overview/index.php
@@ -2,7 +2,7 @@
 	<?php foreach ($players as $player): ?>
 		<a href="<?php echo base_url('viewer/' . $player['playerid']); ?>">
 			<div class="player" id="player<?php echo $player['playerid']; ?>">
-				<img class="player__image" src="/theduely/media/images/avatars/<?php echo $player['number']; ?>.png">
+				<img class="player__image" src="<?php echo base_url('/media/images/avatars/' . $player['number']); ?>.png">
 				<div class="player__name">
 					<?php echo $player['name']; ?>
 				</div>
