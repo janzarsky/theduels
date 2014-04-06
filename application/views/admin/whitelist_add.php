b2cb5a75366909ec6029a67982faa9aeb876d7ca
janzarsky <jan.zarsky@gmail.com>
2014-04-06 19:53:10 +0200

    Syntax fixes in views

--- application/views/admin/whitelist_add.php
+++ application/views/admin/whitelist_add.php
@@ -1,13 +1,17 @@
 <form method="post" action="<?php echo base_url('admin/whitelist_add_submit'); ?>">
-	<div class="field__header">
-		IP:
+	<div class="field">
+		<header class="field__header">
+			IP:
+		</header>
+		<input class="field__text" type="text" name="ip">
 	</div>
-	<input class="field__text" type="text" name="ip">
-		
-	<div class="field__header">
-		Jméno:
+	
+	<div class="field">
+		<header class="field__header">
+			Jméno:
+		</header>
+		<input class="field__text" type="text" name="name">
 	</div>
-	<input class="field__text" type="text" name="name">
 	
 	<input class="field__submit" type="submit" value="Přidat">
 </form>
