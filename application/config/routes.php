48207a4757c49327b16c739c929b0a252046d090
janzarsky <jan.zarsky@gmail.com>
2014-04-06 12:00:07 +0200

    Routes fix

--- application/config/routes.php
+++ application/config/routes.php
@@ -43,9 +43,6 @@ $route['viewer/data/(:num)'] = "viewer/data/$1";
 
 $route['control/(:num)'] = "control/index/$1";
 
-$route['admin/(:any)'] = "admin/$1";
-$route['admin/(:any)'] = "admin/$1";
-
 $route['default_controller'] = "viewer";
 $route['404_override'] = '';
 
