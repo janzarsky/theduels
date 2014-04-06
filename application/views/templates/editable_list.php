248bfdbccc0b8fe84e6f3af2f23388078504264a
janzarsky <jan.zarsky@gmail.com>
2014-04-06 14:03:04 +0200

    Created IP whitelist page

--- application/views/templates/editable_list.php
+++ application/views/templates/editable_list.php
@@ -1,10 +1,11 @@
-<div class="list">
-	<header class="list__header">
+<div class="content">
+	<header class="content__header">
 		<?php echo $header; ?>
 	</header>
-	<ul class="list__list">
-		<?php foreach ($items as $item): ?>
-			<a href="<?php echo base_url($item['url']); ?>">
+	
+	<section class="list">
+		<ul class="list__list">
+			<?php foreach ($items as $item): ?>
 				<li class="list__list__item">
 					<?php if (isset($item['image_url'])) : ?>
 						<img src="<?php echo base_url($item['image_url']); ?>.png" height="48">
@@ -12,17 +13,40 @@
 					
 					<?php echo $item['label']; ?>
 				</li>
-			</a>
-		<?php endforeach; ?>
-		
-		<li class="list_list_add">
-			<form method="post">
-				<div class="form__fieldheader">IP:</div>
-				<input type="text" name="ip">
-				<div class="form__fieldheader">Jméno:</div>
-				<input type="text" name="name">
-				<input type="submit" value="Přidat">
-			</form>
-		</li>
-	</ul>
+			<?php endforeach; ?>
+		</ul>
+	</section>
+	
+	<section class="add">
+		<form method="post" action="<?php echo base_url($add_url); ?>">
+			<div class="field__header">
+				IP:
+			</div>
+			<input class="field__text" type="text" name="ip">
+				
+			<div class="field__header">
+				Jméno:
+			</div>
+			<input class="field__text" type="text" name="name">
+			
+			<input class="field__submit" type="submit" value="Přidat">
+		</form>
+	</section>
+	
+	<section class="delete">
+		<form method="post" action="<?php echo base_url($delete_url); ?>">
+			<div class="field__header">
+				Jméno:
+			</div>
+			<select class="field__select" name="id">
+				<?php	foreach ($items as $item) : ?>
+					<option value="<?php echo $item['id']; ?>">
+						<?php echo $item['label']; ?>
+					</option>
+				<?php endforeach; ?>
+			</select>
+			
+			<input class="field__submit" type="submit" value="Odebrat">
+		</form>
+	</section>
 </div>
