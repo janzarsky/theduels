0ab19b084061b912bde55b6327002139ee426dd8
janzarsky <jan.zarsky@gmail.com>
2014-04-14 16:13:13 +0200

    Skills page moved to controller setup

--- application/models/setup_model.php
+++ application/models/setup_model.php
@@ -32,4 +32,30 @@ class Setup_model extends CI_Model {
 		
 		$this->db->delete('ip_whitelist', array('id' => $id));
 	}
+	
+	public function get_skills_as_items() {
+		return $this->db
+			->select('name as label, id as id')
+			->from('skills')
+			->get()->result_array();
+	}
+	
+	public function add_skill($name = false) {
+		if ($name === false)
+			throw new Exception('empty');
+		
+		$data = array(
+			'name' => $name
+		);
+		
+		$this->db->insert('skills', $data);
+	}
+	
+	public function delete_skill($id = false) {
+		if ($id === false)
+			throw new Exception('empty');
+		
+		$this->db->delete('games', array('skill_id' => $id));
+		$this->db->delete('skills', array('id' => $id));
+	}
 }
