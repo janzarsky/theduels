a82a683b3c69e4126328cf466831713dac0fc882
janzarsky <jan.zarsky@gmail.com>
2014-04-15 20:53:18 +0200

    Fixed editable list

--- application/views/templates/editable_list.php
+++ application/views/templates/editable_list.php
@@ -4,19 +4,17 @@
 	</header>
 	
 	<div class="main">
-		<section class="list">
-			<ul class="list__list">
-				<?php foreach ($items as $item): ?>
-					<li class="list__list__item">
-						<?php if (isset($item['image_url'])) : ?>
-							<img src="<?php echo base_url($item['image_url']); ?>.png" height="48">
-						<?php endif; ?>
-						
-						<?php echo $item['label']; ?>
-					</li>
-				<?php endforeach; ?>
-			</ul>
-		</section>
+		<ul class="list">
+			<?php foreach ($items as $item): ?>
+				<li class="list__item">
+					<?php if (isset($item['image_url'])) : ?>
+						<img src="<?php echo base_url($item['image_url']); ?>.png" height="48">
+					<?php endif; ?>
+					
+					<?php echo $item['label']; ?>
+				</li>
+			<?php endforeach; ?>
+		</ul>
 	</div>
 	
 	<div class="aside">
