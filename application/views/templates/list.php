fa04962d34a1f6f10a3a80743360874bb027eee6
janzarsky <jan.zarsky@gmail.com>
2014-04-15 20:48:51 +0200

    Changed list styling

--- application/views/templates/list.php
+++ application/views/templates/list.php
@@ -3,11 +3,11 @@
 		<?php echo $header; ?>
 	</header>
 	
-	<div class="list">
-		<ul class="list__list">
+	<div class="main main--center">
+		<ul class="list">
 			<?php foreach ($items as $item): ?>
 				<a href="<?php echo base_url($item['url']); ?>">
-					<li class="list__list__item">
+					<li class="list__item">
 						<?php if (isset($item['image_url'])) : ?>
 							<img src="<?php echo base_url($item['image_url']); ?>.png" height="48">
 						<?php endif; ?>
