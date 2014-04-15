b70b9bc5a9074cda77e89362b69082b69c6d9c23
janzarsky <jan.zarsky@gmail.com>
2014-04-15 20:52:03 +0200

    Refactorized structure of views

--- application/views/control/index.php
+++ application/views/control/index.php
@@ -3,49 +3,51 @@
 		<?php echo $game_name; ?>
 	</header>
 	
-	<form action="<?php echo base_url('control/submit/' . $game_id); ?>" method="post">
-		<input type="hidden" name="game_id" value="<?php echo $game_id; ?>">
-		
-		<div class="field" id="player1">
-			<header class="field__header">
-				hráč 1
-			</header>
-			<select class="field__select" name="player_1_id" autofocus>
-				<?php	foreach ($players as $player) : ?>
-					<option class="field__select__option" value="<?php echo $player['id']; ?>">
-						<?php echo $player['name']; ?>
+	<div class="main main--center">
+		<form action="<?php echo base_url('control/submit/' . $game_id); ?>" method="post">
+			<input type="hidden" name="game_id" value="<?php echo $game_id; ?>">
+			
+			<div class="field" id="player1">
+				<header class="field__header">
+					hráč 1
+				</header>
+				<select class="field__select" name="player_1_id" autofocus>
+					<?php	foreach ($players as $player) : ?>
+						<option class="field__select__option" value="<?php echo $player['id']; ?>">
+							<?php echo $player['name']; ?>
+						</option>
+					<?php endforeach; ?>
+				</select>
+			</div>
+			
+			<div class="field">
+				<header class="field__header">
+					skóre
+				</header>
+				<select class="field__select" name="score">
+					<option class="field__select__option" value="2">
+						2:0
 					</option>
-				<?php endforeach; ?>
-			</select>
-		</div>
-		
-		<div class="field">
-			<header class="field__header">
-				skóre
-			</header>
-			<select class="field__select" name="score">
-				<option class="field__select__option" value="2">
-					2:0
-				</option>
-				<option class="field__select__option" value="1">
-					1:1
-				</option>
-			</select>
-		</div>
-		
-		<div class="field" id="player2">
-			<header class="field__header">
-				hráč 2
-			</header>
-			<select class="field__select" name="player_2_id">
-				<?php	foreach ($players as $player) : ?>
-					<option class="field__select__option" value="<?php echo $player['id']; ?>">
-						<?php echo $player['name']; ?>
+					<option class="field__select__option" value="1">
+						1:1
 					</option>
-				<?php endforeach; ?>
-			</select>
-		</div>
-		
-		<input type="submit" class="field__submit" value="OK"/>
-	</form>
+				</select>
+			</div>
+			
+			<div class="field" id="player2">
+				<header class="field__header">
+					hráč 2
+				</header>
+				<select class="field__select" name="player_2_id">
+					<?php	foreach ($players as $player) : ?>
+						<option class="field__select__option" value="<?php echo $player['id']; ?>">
+							<?php echo $player['name']; ?>
+						</option>
+					<?php endforeach; ?>
+				</select>
+			</div>
+			
+			<input type="submit" class="field__submit" value="OK"/>
+		</form>
+	</div>
 </div>
