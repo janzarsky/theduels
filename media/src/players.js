a2c39f2b424babd0f801aca9914ab72c240cfc0e
janzarsky <jan.zarsky@gmail.com>
2014-04-17 13:35:11 +0200

    Deleted development files

--- media/src/players.js
+++
@@ -1,7 +0,0 @@
-$('.images').on('click', '.images__option', function () {
-	var t = $(this);
-	t.addClass('images__option--selected');
-	t.siblings().removeClass('images__option--selected');
-	
-	$('#avatar__select').val(t.attr('value'));
-});
