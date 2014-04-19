119471610445d93033ff5654b88a5642dcf4b4dc
janzarsky <jan.zarsky@gmail.com>
2014-04-19 11:24:45 +0200

    Added check to prevent another installation

--- install.php
+++ install.php
@@ -4,8 +4,12 @@ $base = substr($base, 0, strpos($base, '/install.php'));
 
 $htaccess = file_get_contents('htaccess-template');
 
-$htaccess = str_replace('@rewritebase', $base, $htaccess);
-
-file_put_contents('.htaccess', $htaccess);
-
-header('Location: ' . $base . '/install');
+if (strpos(file_get_contents('.htaccess'), '@rewritebase') === false)
+	echo "Systém je už nainstalován";
+else {
+	$htaccess = str_replace('@rewritebase', $base, $htaccess);
+	
+	file_put_contents('.htaccess', $htaccess);
+	
+	header('Location: ' . $base . '/install');
+}
