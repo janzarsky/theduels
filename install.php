8cf87711af5dd47cccdb91923d9e78163b6bb235
janzarsky <jan.zarsky@gmail.com>
2014-04-19 11:31:11 +0200

    Created check to disable multiple installation

--- install.php
+++ install.php
@@ -4,12 +4,8 @@ $base = substr($base, 0, strpos($base, '/install.php'));
 
 $htaccess = file_get_contents('htaccess-template');
 
-if (strpos(file_get_contents('.htaccess'), '@rewritebase') === false)
-	echo "Systém je už nainstalován";
-else {
-	$htaccess = str_replace('@rewritebase', $base, $htaccess);
-	
-	file_put_contents('.htaccess', $htaccess);
-	
-	header('Location: ' . $base . '/install');
-}
+$htaccess = str_replace('@rewritebase', $base, $htaccess);
+
+file_put_contents('.htaccess', $htaccess);
+
+header('Location: ' . $base . '/install');
