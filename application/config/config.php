0bc14d6dbee6655efd8c9d4136d2cb5e69e435cb
janzarsky <jan.zarsky@gmail.com>
2014-04-19 10:50:57 +0200

    Created login controller

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '1c5db3191af518e5a04b256509d591fc';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/controllers/login.php
@@ -0,0 +1,42 @@
+<?php
+
+class Login extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('session');
+		$this->load->helper('url');
+		$this->load->model('login_model');
+	}
+
+	public function index() {
+		if ($this->session->userdata('logged_in'))
+			redirect('setup');
+		
+		$html_header_data['title'] = 'Login';
+		$html_header_data['style'] = 'login.css';
+		$this->load->view('templates/html_header', $html_header_data);
+		
+		$this->load->view('login/index');
+		
+		$this->load->view('templates/html_footer');
+	}
+	
+	public function submit() {
+		if ($this->login_model->is_password_right($this->input->post('password'))) {
+			$this->session->set_userdata('logged_in', '1');
+			
+			redirect('setup');
+		}
+		else
+			$this->redirect_with_message('login', 'Wrong password');
+	}
+	
+	private function redirect_with_message($url, $message) {
+		if (isset($message) || $message != '')
+			redirect(base_url($url) . '?message=' . $message);
+		else
+			redirect(base_url($url) . '?message=success');
+	}
+}
