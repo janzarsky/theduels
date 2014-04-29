ab73d1b3b4c0928c1d674624fea884b4fccd617c
Jan Žárský <jan.zarsky@gmail.com>
2014-04-29 20:19:48 +0200

    Overview is disabled when rank is turned off, fixes #12

--- application/controllers/overview.php
+++ application/controllers/overview.php
@@ -10,23 +10,50 @@ class Overview extends CI_Controller {
 	}
 
 	public function index() {
-		$html_header_data['title'] = 'Přehled';
-		$html_header_data['style'] = 'overview.css';
-		$this->load->view('templates/html_header', $html_header_data);
-		
-		$overview_data['players'] = $this->overview_model->get_players();
-		$this->load->view('overview/index', $overview_data);
-		
-		$html_footer_data['script'] = 'overview.js';
-		$this->load->view('templates/html_footer', $html_footer_data);
+		try {
+			if ($this->overview_model->is_position_visible() == false)
+				throw new Exception('Zobrazení pořadí zakázáno');
+			
+			$html_header_data['title'] = 'Přehled';
+			$html_header_data['style'] = 'overview.css';
+			$this->load->view('templates/html_header', $html_header_data);
+			
+			$overview_data['players'] = $this->overview_model->get_players();
+			$this->load->view('overview/index', $overview_data);
+			
+			$html_footer_data['script'] = 'overview.js';
+			$this->load->view('templates/html_footer', $html_footer_data);
+		}
+		catch (Exception $e) {
+			$this->show_error_page($e);
+		}
 	}
 	
 	public function data() {
-		if ($this->input->get('w') !== false && $this->input->get('h') !== false)
-			$data['json_object'] = $this->overview_model->get_data($this->input->get('w'), $this->input->get('h'));
-		else
-			$data['json_object'] = $this->overview_model->get_data();
+		try {
+			if ($this->overview_model->is_position_visible() == false)
+				throw new Exception('Zobrazení pořadí zakázáno');
+			
+			if ($this->input->get('w') !== false && $this->input->get('h') !== false)
+				$data['json_object'] = $this->overview_model->get_data($this->input->get('w'), $this->input->get('h'));
+			else
+				$data['json_object'] = $this->overview_model->get_data();
+			
+			$this->load->view('templates/json', $data);
+		}
+		catch (Exception $e) {
+			echo "";
+		}
+	}
+	
+	private function show_error_page($error) {
+		$html_header_data['title'] = 'Chyba';
+		$html_header_data['style'] = 'error.css';
+		$this->load->view('templates/html_header', $html_header_data);
 		
-		$this->load->view('templates/json', $data);
+		$data['message'] = $error->getMessage();
+		$this->load->view('templates/error', $data);
+		
+		$this->load->view('templates/html_footer');
 	}
 }
