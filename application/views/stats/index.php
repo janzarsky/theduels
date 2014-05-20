20cab25c6e47a155e9d941fd422a19f117cc619f
Jan Žárský <jan.zarsky@gmail.com>
2014-05-20 11:41:50 +0200

    Add link to viewer in stats table

--- application/views/stats/index.php
+++ application/views/stats/index.php
@@ -10,10 +10,20 @@
 					<th><?php echo $title;?></th>
 				<?php endforeach; ?>
 			</tr>
-			<?php foreach ($table as $row): ?>
+			<?php foreach ($table as $row_id => $row): ?>
 				<tr>
-					<?php foreach ($row as $cell): ?>
-						<td><?php echo $cell;?></td>
+					<?php foreach ($row as $cell_id => $cell): ?>
+						<?php if ($cell_id == 'name'): ?>
+							<td>
+								<a href="<?php echo base_url('viewer/' . $row_id); ?>">
+									<?php echo $cell; ?>
+								</a>
+							</td>
+						<?php else: ?>
+							<td>
+								<?php echo $cell; ?>
+							</td>
+						<?php endif; ?>
 					<?php endforeach; ?>
 				</tr>
 			<?php endforeach; ?>
